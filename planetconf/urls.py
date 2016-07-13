e42ef11cf4e0b37995353e729d055b028840a967
gamze <gamze.canli@infoowl.net>
2016-07-13 12:26:15 +0300

    ornek kod yazildi-helloworld

--- planetconf/urls.py
+++ planetconf/urls.py
@@ -1,9 +1,10 @@
 
 from django.conf.urls import  patterns, include, url
 from django.contrib import admin
-
+from planet.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'planet/$', index),
 
 ]

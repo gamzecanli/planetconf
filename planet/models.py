e2af991884f65a6a901bd4962d65f7607aee58e9
gamze <gamze.canli@infoowl.net>
2016-07-14 11:30:32 +0300

    modeller olusturuldu #2

--- planet/models.py
+++ planet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Planet(models.Model):
+    planet_name = models.CharField(max_length=64)
+    link = models.CharField(max_length=128)
+    planet_directory_name = models.CharField(max_length=64)
+
+
+class Blogger(models.Model):
+    blogger_name = models.CharField(max_length=64)
+    blogger_link = models.CharField(max_length=256)
